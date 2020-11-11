5cb72f4025190f491e6acba9a942c466d7de1c9e
zhoubencheng <1107472286@qq.com>
2020-11-11 23:39:33 +0800

    小明提交了代码

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,7 +11,10 @@ import android.view.MenuItem
  * 作者邮箱： mappstore@163.com
  * 功能描述： 
  * 类    名： 增加了注释
- * 备    注： 
+ * 备    注：
+ *
+ *
+ * 这个是小明的分支
  */
 
 class MainActivity : AppCompatActivity() {
