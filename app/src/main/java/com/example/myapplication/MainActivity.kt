ce691e9050620ed3d2f5d117a6f988454a334de9
benchengzhou <1107472286@qq.com>
2020-11-12 10:23:26 +0800

    dev提交代码

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,11 +11,8 @@ import android.view.MenuItem
  * 作者邮箱： mappstore@163.com
  * 功能描述： 
  * 类    名： 增加了注释
- * 备    注：
+ * 备    注： 这个是master的分支
  *
- *
- * 这个是小明的分支
- * 备    注： dev -> master
  *
  * master提交了代码
  */
